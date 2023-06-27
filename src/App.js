e6541f2de3fe12860b073c0ec2c4ef41fc88faa6
Hannington Kizza <82478265+HanzKizza@users.noreply.github.com>
2023-06-27 10:21:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import Greetings from './Components/Greetings'
 function App() {
+  const tasks = [
+    {
+      id: 1,
+      text: "Meeting with elon",
+      day: "16th Aug",
+      reminder: true
+    },
+    {
+      id: 2,
+      text: "Lunch with CMX",
+      day: "15th May",
+      reminder: false
+    },
+    {
+      id: 3,
+      text: "Pick up son from school",
+      day: "everyday",
+      reminder: true
+    }
+  ]
   return (
     <div>
-      <Greetings name="Mark" wishes = "Have a good day"/>
+      {
+        tasks.map((task) => (<h1>{task.text}</h1>))
+      }
     </div>
   );
 }
